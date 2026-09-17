b9a4834ae6d0c5aefa375a75355231182fefee88
Riccardo Spolaor <986234+riki8686@users.noreply.github.com>
2026-09-17 20:38:56 +0800

    new website distro

--- src/content/awards.js
+++
@@ -1,6 +0,0 @@
-export const awards = [
-  "Best Paper Award, ACM Workshop on AI in Networks (WAIN 2018), for \"MalAlert: Detecting Malware in Large-Scale Network Traffic Using Statistical Features\"",
-  "First Prize, Notte dei Brevettatori 2015 (Padua, Italy), for the patent \"Method for Recognizing if a User of an Electronic Terminal is a Human or a Robot\"",
-  "Fifth Place, CLUSIT Best Thesis Award 2014 (Milan, Italy), for the MSc thesis \"Can't You Hear Me Knocking: Identifying Smartphone User Actions with Apps via Traffic Analysis\"",
-  "First Place, CLUSIT Best Thesis Award 2015 (Milan, Italy), as co-author (with Claudio Guarisco as first author)",
-];
